0543c34f87b4ffd314b14743a0c1c9ba89bb1778
Jon Williams <jon@jonathannen.com>
2013-07-20 19:41:30 +1000

    Messing around with styles

--- canary.rb
+++ canary.rb
@@ -1,5 +1,32 @@
 require 'sinatra'
 
 get '/' do
-  "<!DOCTYPE html><html><head></head><body><h1>Chirp! Chirp!</h1></body></html>"
+<<-HTML
+<!DOCTYPE html>
+ <html>
+    <head>
+      <title>Canary Sinatra</title>
+      <style type='text/css'>
+        html { height: 100%; }
+        body { 
+          background: url('/canary.jpg') no-repeat center center;
+          background-size: cover;
+        }
+        h1 {      
+          color: #ffd500;
+          font-family: 'Helvetica Neue',Helvetica, sans-serif;
+          font-size: 40px; font-weight: bold; line-height: 1em;
+          position: absolute; top: 50%; left: 50%;
+          text-shadow: 0px 0px 0 rgb(210,168,-45),1px 1px 0 rgb(197,155,-58),2px 2px 0 rgb(183,141,-72),3px 3px 0 rgb(169,127,-86), 4px 4px 0 rgb(155,113,-100),5px 5px 4px rgba(0,0,0,0.5),5px 5px 1px rgba(0,0,0,0.5),0px 0px 4px rgba(0,0,0,.2);
+          text-align: center;
+          margin: -25px -200px;
+          width: 400px; height: 50px;          
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Chirp! Chirp!</h1>
+    </body>
+  </html>
+HTML
 end
--- unicorn.rb
+++ unicorn.rb
@@ -1,15 +1,18 @@
 require 'fileutils'
 
 dir = File.dirname(File.expand_path(File.realdirpath(__FILE__)))
+production = ENV['RACK_ENV'] == 'production'
 %w{log tmp/pids tmp/sockets}.each { |v| FileUtils.mkdir_p(File.join(dir, v)) }
 
 worker_processes 2
 working_directory dir
 timeout 30
 
-port = ENV['RACK_ENV'] == 'production' ? File.join(dir, 'tmp/sockets/unicorn.sock') : 4567
+port = production ? File.join(dir, 'tmp/sockets/unicorn.sock') : 4567
 listen port, backlog: 64
 
 pid File.join(dir, 'tmp/pids/unicorn.pid')
-stderr_path File.join(dir, 'log/unicorn.stderr.log')
-stdout_path File.join(dir, 'log/unicorn.stdout.log')
+if production
+  stderr_path File.join(dir, 'log/unicorn.stderr.log')
+  stdout_path File.join(dir, 'log/unicorn.stdout.log')
+end
